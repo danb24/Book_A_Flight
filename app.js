d16d0443c4d28ccae5fcf0a74386f52eb823619a
danb24 <135611724+danb24@users.noreply.github.com>
2023-08-07 21:29:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.use(express.static('public')) 
+const {tlvflight}= require('./model/mongoDB')
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
   res.send('')
 })
 
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`)
+})
+
+
+app.get('/flights', async (req, res) => {
+  const flights = await tlvflight()
+  res.json(flights)
 })
