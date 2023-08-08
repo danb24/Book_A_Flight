a44a3a138b6062e079f794d6db6a39c6fe5fafd9
barziv555 <135611106+barziv555@users.noreply.github.com>
2023-08-08 15:54:14 +0300

    script.js

--- public/js/script.js
+++ public/js/script.js
@@ -10,7 +10,45 @@ const fetchFunction = (html) => {
 async function fetchFlights(){
     const respone = await fetch ('/flights')
     const data = await respone.json();
-    console.log(data);
+    const flightlist = document.getElementById('flightlist')
+    flightlist.innerHTML=``
+    let flight = ''
 
-    
+    data.forEach((flights) =>{
+        flight +=`<tr>
+    <td>${flights.flight_number}</td>
+    <td>${flights.from}</td>
+    <td>${flights.dest}</td>
+    <td>${flights.date}</td>
+    <td>${flights.price}</td>
+    <td>${flights.company}</td>
+    <tr>`})
+    flightlist.innerHTML=flight;
+}
+
+
+
+
+
+
+async function filterFlights() {
+    const from = document.getElementById('from').value;
+    const dest = document.getElementById('dest').value;
+
+    const response = await fetch('/flights?from=' + from + '&dest=' + dest);
+    const data = await response.json();
+    const flightList = document.getElementById('flightlist');
+    flightList.innerHTML = ``
+    let filtered = ''
+
+    data.forEach((flights) =>{
+        filtered +=`<tr>
+    <td>${flights.flight_number}</td>
+    <td>${flights.from}</td>
+    <td>${flights.dest}</td>
+    <td>${flights.date}</td>
+    <td>${flights.price}</td>
+    <td>${flights.company}</td>
+    <tr>`})
+    flightList.innerHTML=filtered;
 }
